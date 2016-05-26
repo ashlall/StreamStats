603c9aca83eb699bd18b875991e3fa7e78b392ec
EmilyFarrow <farrow_e1@denison.edu>
2016-05-26 15:59:12 -0400

    update for min

--- SourceCode/BasicStats/BasicStats.cpp
+++ SourceCode/BasicStats/BasicStats.cpp
@@ -1,1 +1,19 @@
+#include "BasicStats.h"
 
+BasicStats::BasicStats()
+{
+  sum = 0;
+  N = 0;
+  min = 0x80000000 // smallest number possible?
+}
+
+BasicStats::insert(double num)
+{
+  if (num < min)
+    min = num;
+}
+
+BasicStats::MIN()
+{
+  return min;
+}
