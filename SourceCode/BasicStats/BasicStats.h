7782931cee7bb31b6e61162568e4e31cde71ed06
Drown1994 <larry812junbo@gmail.com>
2016-05-26 15:47:40 -0400

    Update BasicStats.h

--- SourceCode/BasicStats/BasicStats.h
+++ SourceCode/BasicStats/BasicStats.h
@@ -4,3 +4,19 @@
 #include <iostream>
 
 using namespace std;
+
+class BasicStats
+{
+  private:
+    int data, sum, N;
+  public: 
+    BasicStats(int num, int counter);
+    ~BasicStats();
+    BasicStats mean();
+    BasicStats SD(mean, N, data);
+    BasicStats MAX();
+    BasicStats MIN();
+}
+
+#include "BasicStats.cpp"
+#endif
