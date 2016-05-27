78242c7d9f7a5014772905ba862d3a5aba9cee4b
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 16:20:36 -0400

    initialized Reservior sampling unit test

---
+++ Test/Reservoir_test.cpp
@@ -0,0 +1,16 @@
+// unit tests for Reservoir Sampling class
+
+#include<cassert>
+#include "../SourceCode/Quantiles/ReservoirSampling.h"
+#include <iostream>
+using namespace std;
+
+void test_constructor()
+{
+  ReservoirSampling a(10);
+}
+
+int main()
+{
+  return 0;
+}
