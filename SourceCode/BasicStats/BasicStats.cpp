8b49035b002d7d979e220b045799f457b2bf66a9
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 15:21:34 -0400

    update average

--- SourceCode/BasicStats/BasicStats.cpp
+++ SourceCode/BasicStats/BasicStats.cpp
@@ -6,15 +6,18 @@ BasicStats::BasicStats()
   N = 0;
   min = 0; // smallest number possible?
   max = 0;
+  count=0;
 }
 
 void BasicStats::insert(double num)
 {
   N = N+1;
-  min = MIN();
-   max = BasicStats.MAX(num);
+  sum=sum+num;
+  //min = MIN();
+  //max = BasicStats.MAX(num);
 }
 
+/*
 double BasicStats::MIN()
 {
   if (N == 1)
@@ -31,4 +34,10 @@ double BasicStats ::Max(double num)
 	  return num;
 	else 
 	  return max; 
+}*/
+
+double Basic Stats:: Average(double num)
+{
+	return sum/N;
+
 }
