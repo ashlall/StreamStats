02c423593bb81df9f4c34b2474c539c6af7a720e
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 10:10:47 -0400

    initialize BasicStats unit tests

--- Test/BasicStats_test.cpp
+++ Test/BasicStats_test.cpp
@@ -1,1 +1,32 @@
+// Unit tests for the BasicStats class
 
+#include "../SourceCode/BasicStats/BasicStats.h"
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+void test_constructor()
+{
+  
+}
+
+void test_insert()
+{
+  BasicStats b;
+  b.insert(3);
+  b.insert(7);
+  b.insert(1);
+  assert(b.MIN()==1);
+}
+
+void test_MIN()
+{
+
+}
+
+int main()
+{
+  test_constructor();
+  test_insert();
+  test_MIN();
+}
