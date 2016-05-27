3707ecf465919fbb2dba0dc4f29c532621e39180
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 15:54:30 -0400

    initialize QuantileSketch.h

--- SourceCode/Quantiles/QuantileSketch.h
+++ SourceCode/Quantiles/QuantileSketch.h
@@ -1,1 +1,36 @@
+// QuantileSketch.h
 
+#ifndef QUANTILE_SKETCH
+#define QUANTILE_SKETCH
+
+#include <iostream>
+using namespace std;
+
+class  QuantileSketch
+{
+ public:
+  QuantileSketch() {};
+  ~QuantileSketch() {};
+  virtual void insert(double x);
+  virtual double getQuantile(double q);
+  int reverseQuantile(double r, int n);
+};
+
+//#include "QuantileSketch.cpp"
+
+#endif QUANTILE_SKETCH
+
+int QuantileSketch::reverseQuantile(double r, int n)
+{
+  int lo=0, hi= n-1;
+  while (lo < hi)
+  {
+    int mid = (lo+hi)/2;
+    double value = getQuantile(((double)mid)/n);
+    if (value <= r)
+      lo = mid+1;
+    else
+      hi = mid;
+  }
+  return lo;
+}
