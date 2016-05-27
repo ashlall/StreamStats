73fb1c87e115f7ece4b27312d53e731b43d910b6
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 12:22:35 -0400

    update MIN unit tests

--- Test/BasicStats_test.cpp
+++ Test/BasicStats_test.cpp
@@ -10,23 +10,29 @@ void test_constructor()
   
 }
 
-void test_insert()
-{
-  BasicStats b;
-  b.insert(3);
-  b.insert(7);
-  b.insert(1);
-  assert(b.MIN()==1);
-}
-
 void test_MIN()
 {
+  BasicStats a; // tests when there is one item in the stream
+  a.insert(3);
+  assert(a.MIN()==3);
 
+  BasicStats b; // tests with 100 items in the stream in ascending order
+  for (int i=1;i<100;i++)
+    b.insert(i);
+  assert(b.MIN()==1);
+
+  BasicStats c; // tests with one million items in the stream in descending order 
+  for (int i=10;i>=1;i++)
+  {
+    c.insert(i);
+    cout<<c.MIN()<<endl;
+  }
+  cout<<c.MIN()<<endl;
+  //  assert(c.MIN()==1);
 }
 
 int main()
 {
   test_constructor();
-  test_insert();
   test_MIN();
 }
