2fef389c16cc956976b4d9e8c5affa62c2c35c19
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 10:18:05 -0400

    added ;

--- SourceCode/BasicStats/BasicStats.cpp
+++ SourceCode/BasicStats/BasicStats.cpp
@@ -4,7 +4,7 @@ BasicStats::BasicStats()
 {
   sum = 0;
   N = 0;
-  min = 0x80000000 // smallest number possible?
+  min = 0x80000000; // smallest number possible?
 }
 
 void BasicStats::insert(double num)
