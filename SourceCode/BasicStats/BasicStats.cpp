d35fa4555dcf587701a9cfcfbb1e0f7d8b5c4355
Drown1994 <larry812junbo@gmail.com>
2016-05-27 13:54:51 -0400

    MAX

--- SourceCode/BasicStats/BasicStats.cpp
+++ SourceCode/BasicStats/BasicStats.cpp
@@ -5,6 +5,7 @@ BasicStats::BasicStats()
   sum = 0;
   N = 0;
   min = 0; // smallest number possible?
+  max = 0;
 }
 
 void BasicStats::insert(double num)
@@ -14,9 +15,18 @@ void BasicStats::insert(double num)
     min = num;
   if (num < min)
     min = num;
+   max = BasicStats.MAX(num);
 }
 
 double BasicStats::MIN()
 {
   return min;
+}
+
+double BasicStats ::Max(double num)
+{
+	if (num >= max)
+		return num
+	else 
+		return max 
 }
