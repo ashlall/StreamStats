49ee45d15c70bfa55585476bcf049fab51be5a22
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 15:21:19 -0400

    update average

--- SourceCode/BasicStats/BasicStats.h
+++ SourceCode/BasicStats/BasicStats.h
@@ -9,15 +9,16 @@ class BasicStats
 {
   private:
     double data, sum, min;
-    int N;
+    long long int N;
   public: 
     BasicStats();
     ~BasicStats() {};
     void insert(double num);
-    double mean();
-    double SD(double mean, int N, double data);
-    double MAX();
-    double MIN();
+    //double mean();
+   // double SD(double mean, int N, double data);
+   // double MAX();
+   // double MIN();
+    double Average();
 };
 
 #include "BasicStats.cpp"
