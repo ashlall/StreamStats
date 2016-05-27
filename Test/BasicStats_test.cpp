3644ec50733259baac0a005ea05568a394315cdc
Drown1994 <larry812junbo@gmail.com>
2016-05-27 15:17:19 -0400

    MAX

--- Test/BasicStats_test.cpp
+++ Test/BasicStats_test.cpp
@@ -10,6 +10,7 @@ void test_constructor()
   
 }
 
+/*
 void test_MIN()
 {
   BasicStats a; // tests when there is one item in the stream
@@ -31,9 +32,40 @@ void test_MIN()
     d.insert(i*3);
   assert(d.MIN()==3);
 }
+*/
+
+void test_MAX()
+{
+/*
+	BasicStats e;
+	e.insert(5);
+	e.insert(109);
+	e.insert(10);
+	cout << e.MAX() <<endl;
+*/
+ BasicStats a; // tests when there is one item in the stream
+  a.insert(3);
+  assert(a.MAX()==3);
+
+  BasicStats b; // tests with 100 items in the stream in ascending order
+  for (int i=1;i<100;i++)
+    b.insert(i);
+  assert(b.MAX()==99);
+
+  BasicStats c; // tests with one million items in the stream in descending order 
+  for (int i=1000000;i>=1;i--)
+    c.insert(i);
+  assert(c.MAX()==1000000);
+
+  BasicStats d; // tests the stream in ascending order
+  for (int i=1;i<=100000;i++)
+    d.insert(i*3);
+  assert(d.MAX()==300000);
+}
 
 int main()
 {
   test_constructor();
-  test_MIN();
+  //test_MIN();
+  test_MAX();
 }
