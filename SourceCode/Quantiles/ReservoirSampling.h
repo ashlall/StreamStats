5fc96ca0d34b199fc875da7fb4879eb05ff932bb
EmilyFarrow <farrow_e1@denison.edu>
2016-05-27 15:58:34 -0400

    initialize ReservoirSketch.h

--- SourceCode/Quantiles/ReservoirSampling.h
+++ SourceCode/Quantiles/ReservoirSampling.h
@@ -1,1 +1,53 @@
+// ReservoirSampling.h
 
+#include "QuantileSketch.h"
+#include <iostream>
+#include <algorithm>
+#include <rand>
+using namespace std;
+
+#ifndef RESERVOIR_SAMPLING
+#define RESERVOIR_SAMPLING
+
+class ReservoirSampling: public QuantileSketch
+{
+ public:
+  ReservoirSampling(int items);
+  ~ReservoirSampling();
+  void insert(double x);
+  double getQuantile(double f);
+ protected:
+  int n,k;
+  double []s;
+};
+
+#include "ReservoirSampling.cpp"
+
+#endif RESERVOIR_SAMPLING
+
+ReservoirSampling::ReservoirSampling(int items)
+{
+  k = items;
+  s = new double[k];
+  n = 0;
+}
+
+void ReservoirSampling::insert(double x)
+{
+  n += 1;
+  if (n <= k)
+    s[n-1] = x;
+  else if ((rand % n + 1) <= k)
+    s[(rand % k)] = x;
+}
+
+double ReservoirSampling::getQuantile(double f)
+{
+  sort(s,s+k);
+  if (f >= 1)
+    return s[k-1];
+  else if (f < 0)
+    return s[0];
+  else
+    return s[(int)(f*k)];
+}
