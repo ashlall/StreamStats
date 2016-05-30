409fcd0c1d82cdf90f5dc726e7daba1847b8b943
Farhan Zaki <zaki_f1@denison.edu>
2016-05-30 11:51:17 -0400

    Edited

--- SourceCode/Quantiles/ReservoirSampling.h
+++ SourceCode/Quantiles/ReservoirSampling.h
@@ -13,41 +13,12 @@ class ReservoirSampling: public QuantileSketch
 {
  public:
   ReservoirSampling(int items);
-  //~ReservoirSampling();
+  ~ReservoirSampling();
   void insert(double x);
   double getQuantile(double f);
  protected:
   int n,k;
   double *s;
 };
-
-//#endif
-
-ReservoirSampling::ReservoirSampling(int items)
-{
-  k = items;
-  s = new double[k];
-  n = 0;
-}
-
-void ReservoirSampling::insert(double x)
-{
-  n += 1;
-  if (n <= k)
-    s[n-1] = x;
-  else if ((rand() % n + 1) <= k)
-    s[(rand() % k)] = x;
-}
-
-double ReservoirSampling::getQuantile(double f)
-{
-  sort(s,s+k);
-  if (f >= 1)
-    return s[k-1];
-  else if (f < 0)
-    return s[0];
-  else
-    return s[(int)(f*k)];
-}
-
+#include"ReservoirSampling.cpp"
 #endif
