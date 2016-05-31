5c97aee4688a5e265ede8b3001d566122e03d407
Drown1994 <larry812junbo@gmail.com>
2016-05-31 02:46:53 -0400

    rough test of reservior sampling

--- Test/Reservoir_test.cpp
+++ Test/Reservoir_test.cpp
@@ -5,15 +5,19 @@
 #include <iostream>
 using namespace std;
 
-void test_constructor()
+void test_constructor() //more formal test is needed.. involve the consideration of error term 
 {
-  ReservoirSampling a(10);
-  a.insert(1);
-  a.insert(2);
-  a.insert(3);
+  ReservoirSampling a(100);
+  for (int i=1;i<=10000;i++)
+  {
+  	a.insert(i);
+  }
+  cout << a.getQuantile(0.5) << endl; //check the median 
+  cout << a.reverseQuantile(5000, 100) <<endl; //reverse look-up of the approximate index of 5000
 }
 
 int main()
 {
+ test_constructor();
   return 0;
 }
