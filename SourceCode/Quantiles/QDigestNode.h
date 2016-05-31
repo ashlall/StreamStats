677276c68b48edf0b8ac59b3609362f16b7017b0
EmilyFarrow <farrow_e1@denison.edu>
2016-05-31 09:32:50 -0400

    initialize QDigestNode.h

---
+++ SourceCode/Quantiles/QDigestNode.h
@@ -0,0 +1,19 @@
+// QDigestNode.h
+
+#ifndef QDIGESTNODE_H
+#define QDIGESTNODE_H
+
+class QDigestNode
+{
+ public:
+  QDigestNode(int l,int u)
+  {
+    left = right = parent = NULL; // null node?
+    count = 0;
+    lower = l;
+    upper = u;
+  }
+  ~QDigestNode() {};
+  QDigestNode *left, *right, *parent;
+  int count, lower, upper;
+};
