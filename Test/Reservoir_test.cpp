3e53d44b8c4817da3230bc4e23b44342ebb87d67
Farhan Zaki <zaki_f1@denison.edu>
2016-06-01 10:03:03 -0400

    Made more effecient

--- Test/Reservoir_test.cpp
+++ Test/Reservoir_test.cpp
@@ -8,7 +8,10 @@ void test_reservoir1()
 {
   ReservoirSampling a(1000);
   ReservoirSampling b(1000);
-  int quantA,quantB;bool condA,condB;
+  ReservoirSampling c(1000);
+  int quantA,quantB,quantC;
+  bool condA,condB,condC;
+  double upper,lower;
   int n=10000000;
   
   for (int i=n;i>=1;i--)
@@ -17,7 +20,9 @@ void test_reservoir1()
   }
   for (int i=0;i<=n;i++)
   {
+  	int rand_num=rand()%n;
   	b.insert(i); //inserting in ascending order
+  	c.insert(rand_num); //inserting random numbers of range 0-10 million
   }
   
   for (float i=0;i<=100;i++)
@@ -26,47 +31,29 @@ void test_reservoir1()
   	  
   	  quantA=a.getQuantile(interval); //getting quantile for each probability interval
   	  quantB=b.getQuantile(interval);//getting quantile for each probability interval
+  	  quantC=c.getQuantile(interval);//getting quantile for each probability interval
   	  
-  	  condA= ((interval-0.04)*n <=quantA) && ((interval+0.04)*n>=quantA);
+  	  lower=(interval-0.05)*n;
+  	  upper=(interval+0.05)*n;
+  	  
+  	  condA= ((lower<=quantA) && (upper>=quantA));
   	  //checking if quantile is within error range
   	  
-  	  condB= ((interval-0.04)*n <=quantB) && ((interval+0.04)*n>=quantB);
+  	 condB= ((lower<=quantB) && (upper >=quantB));
+  	  //checking if quantile is within error range
+  	  
+  	  condC= ((lower<=quantC) && (upper>=quantC));
   	  //checking if quantile is within error range
   	  
   	  assert(condA==true);
   	  assert(condB==true);
-  }
-  
+  	  assert(condC==true);
+  } 
   	  
 }
-void test_reservoir2()
-{
-  ReservoirSampling a(1000);
-  int quant;bool cond;
-  int n=10000000;
-  
-  for (int i=1;i<=n;i++)
-  {	
-  	int rand_num=rand()%10000000;
-  	a.insert(rand_num); //inserting random numbers of range 0-10 million
-  }
-  for (float i=0;i<=100;i++)
-  {
-  	  float interval=i/100;//getting each probabilty interval
-  	  quant=a.getQuantile(interval);//getting quantile for each probability interval
-  	  
-  	  cond= ((interval-0.04)*n <=quant) && ((interval+0.04)*n>=quant);
-  	  //checking if quantile is within error range
-  	  
-  	  assert(cond==true);
-  }
-	
-}
-
 
 int main()
 {
- test_reservoir1(); //tests for data in ascending and descending order. 
- test_reservoir2(); //tests for data inserted in random order
+ test_reservoir1();
   return 0;
 }
