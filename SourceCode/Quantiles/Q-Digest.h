3fdb45def00bfa0e88045c7ac14ec46a5726239a
Drown1994 <larry812junbo@gmail.com>
2016-06-06 11:12:35 -0400

    update

--- SourceCode/Quantiles/Q-Digest.h
+++ SourceCode/Quantiles/Q-Digest.h
@@ -21,22 +21,24 @@ class QDigest : public QuantileSketch
  private:
   long value2leaf(long x) { return capacity + x; };
   long leaf2value(long id) { return id - capacity; };
-  bool isRoot(long id) { return id == 1; };
-  bool isLeaf(long id) { return id >= capacity; };
-  long sibling(long id) { return (id % 2 == 0) ? (id + 1) : (id - 1); };
-  long parent(long id) { return id / 2; };
-  long leftChild(long id) { return 2 * id; };
-  long rightChild(long id) { return 2 * id + 1; };
-  long doubleToLong(double d) { return (long)(((d + range)/(2 * range)) * precision); };
-  double longToDouble(long l) { return (((double)l)/precision) * 2 * range - range; };
+  bool isRoot(long id) { return id == 1; }; // Check if the input index is root or not. (Root index is 1)
+  bool isLeaf(long id) { return id >= capacity; }; // Check if the input index is leaf or not. 
+  long sibling(long id) { return (id % 2 == 0) ? (id + 1) : (id - 1); };// Return the index of its sibling.
+  long parent(long id) { return id / 2; }; // Return the index of its parent.
+  long leftChild(long id) { return 2 * id; };// Return the index of its left child.
+  long rightChild(long id) { return 2 * id + 1; }; // Return the index of its right child.
+  long doubleToLong(double d) { return (long)(((d + range)/(2 * range)) * precision); }; //Handle the case that 
+  double longToDouble(long l) { return (((double)l)/precision) * 2 * range - range; }; // the original q-diget algorithm can only store integer
   long rangeLeft(long id);
   long rangeRight(long id);
   void offer(long value);
   void compressUpward(long node);
   long get(long node);
   double clamp(double value);
-
-  std::unordered_map<long, long> node2count; //= new std::unordered_map<long, long>();
+  int highestOneBit(long value); 
+  
+  	
+  std::unordered_map<long, long> node2count; //= new std::unordered_map<long, long>(), Map<key, value>
   long size, capacity; 
   double k; // compression factor
   double range; // assume all value in range [-range, range]
