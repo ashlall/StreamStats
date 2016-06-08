00f07e8ab1b678d3be79d504ddd89f5dd443a66e
EmilyFarrow <farrow_e1@denison.edu>
2016-06-08 11:40:10 -0400

    organized functions

--- SourceCode/Quantiles/Q-Digest.h
+++ SourceCode/Quantiles/Q-Digest.h
@@ -21,12 +21,11 @@ class QDigest : public QuantileSketch
   QDigest(const QDigest& q);
   QDigest& operator=(const QDigest& q);
   ~QDigest() {};
+  
   void insert(double x);
   double getQuantile(double f);
 
  private:
-  void copy(const QDigest& q);
-  long clamp(long x);
   long value2leaf(long x) { return capacity + x; };
   long leaf2value(long id) { return id - capacity; };
   bool isRoot(long id) { return id == 1; }; // Check if the input index is root or not. (Root index is 1)
@@ -35,24 +34,27 @@ class QDigest : public QuantileSketch
   long parent(long id) { return id / 2; }; // Return the index of its parent.
   long leftChild(long id) { return 2 * id; };// Return the index of its left child.
   long rightChild(long id) { return 2 * id + 1; }; // Return the index of its right child.
+  
   long rangeLeft(long id);
   long rangeRight(long id);
-  void offer(long value);
-  void compressUpward(long node);
-  void compressDownward(long seedNode);
-  void compressFully();
+  long clamp(long x);
   long get(long node);
   int highestOneBit(long value); 
-  std::vector<long*> toAscRanges();
+  
+  void offer(long value);
   void rebuildToCapacity(long newCapacity);  	
+  void compressUpward(long node);
+  void compressFully();
+  void compressDownward(long seedNode);
+  void copy(const QDigest& q);
+
+  std::vector<long*> toAscRanges();
   void delete_ranges(std::vector<long*> ranges);
   static bool compare_ranges(long a[3], long b[3]);
-  	
+
   std::unordered_map<long, long> node2count; //= new std::unordered_map<long, long>(), Map<key, value>
-  long size;
-  long capacity; 
+  long size, capacity, min, max;
   double k; // compression factor
-  long min, max;
 };
 
 #include "Q-Digest.cpp"
