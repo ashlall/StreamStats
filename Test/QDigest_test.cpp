0b0aa4ab6601419b1c5b623ada718d58af731004
Drown1994 <larry812junbo@gmail.com>
2016-06-08 12:00:15 -0400

    test

--- Test/QDigest_test.cpp
+++ Test/QDigest_test.cpp
@@ -3,37 +3,83 @@
 #include<cassert>
 #include "../SourceCode/Quantiles/Q-Digest.h"
 #include <iostream>
+
 using namespace std;
 
 void test_qdigest()
 {
-	//Example from the paper
-	int hi[15] = {1, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8};
-	QDigest a(5);
-	for(int i=1; i<=15; i++)
+
+	int n = 100000; //stream size;
+	int k = 1/2 * n; //compress factor
+	long quantA,quantB,quantC;
+	bool condA,condB,condC;
+    double upper,lower;
+    
+	QDigest a(k);
+	QDigest b(k);
+	QDigest c(k);
+	
+	for (int i = n; i >= 1; i--)
+    {
+  		a.insert(i); //inserting in desceding order
+    }
+    for (int i = 0; i <= n; i++)
+   {
+  		int rand_num=rand()%n;
+  		b.insert(i); //inserting in ascending order
+  		c.insert(rand_num); //inserting random numbers of range 0-10 million
+  	}  
+  	
+  	
+  for (float i=1;i<=100;i++)
+  {
+  	  double interval=i/100; //getting each probabilty interval
+  	  
+  	  cout << "interval: " << interval << endl;
+  	  quantA=a.getQuantile(interval); //getting quantile for each probability interval
+  	  cout << "hi2" <<endl;
+  	  
+  	  quantB=b.getQuantile(interval);//getting quantile for each probability interval
+  	  quantC=c.getQuantile(interval);//getting quantile for each probability interval
+  	  
+  	  lower=(interval-0.05)*n;
+  	  upper=(interval+0.05)*n;
+  	  
+  	  condA= ((lower<=quantA) && (upper>=quantA));
+  	  //checking if quantile is within error range
+  	  
+  	 condB= ((lower<=quantB) && (upper >=quantB));
+  	  //checking if quantile is within error range
+  	  
+  	  condC= ((lower<=quantC) && (upper>=quantC));
+  	  //checking if quantile is within error range
+  	  
+  	  assert(condA==true);
+  	  assert(condB==true);
+  	  assert(condC==true);
+  } 
+    
+    
+	/*
+
+	QDigest a(35000);
+	for(int i=1; i<=100000; i++)
 	{
 		a.insert(i);
 		//cout << a.toString() << endl;
 	}
 	//cout << a.toString() << endl;
 	cout << "median:" << a.getQuantile(0.5) << endl;
-	cout << "next: "<<a.getQuantile(0.1) << endl;
-	cout << "next: "<<a.getQuantile(0.3) << endl;
-	cout << "next: "<<a.getQuantile(0.4) << endl;
+	cout << "0.1: "<<a.getQuantile(0.1) << endl;
+	cout << "0.3: "<<a.getQuantile(0.3) << endl;
+	cout << "0.4: "<<a.getQuantile(0.4) << endl;
+	cout << "0.8: "<<a.getQuantile(0.8) << endl;
+	*/
 }
 
-void test_constructors()
-{
-  QDigest a(3);
-  a.insert(2);
-  
-  //QDigest b(a);
-
-}
 
 int main()
 {
   test_qdigest();
-  //test_constructors();
   return 0;
 }
