837890a80ba494b965d12464b6467f0b8679f2cf
EmilyFarrow <farrow_e1@denison.edu>
2016-06-08 11:24:06 -0400

    cleans up comments and style

--- SourceCode/Quantiles/Q-Digest.cpp
+++ SourceCode/Quantiles/Q-Digest.cpp
@@ -1,5 +1,5 @@
 // Q-Digest.cpp, implemented with a hash table, adapted from: 
-// to do: debug
+// to do: clean
 
 #include <math.h>
 #include <stdlib.h>
@@ -79,74 +79,69 @@ bool QDigest::compare_ranges(long a[3], long b[3])
   if (rightA < rightB)
     return 1;
   else if (rightA > rightB)
-    return NULL;
+    return 0;
   else if (sizeA < sizeB)
     return 1;
   else if (sizeA > sizeB)
-    return NULL;
+    return 0;
   return 0;
 }
 
 void QDigest::offer(long value)
 {
-    if (value < 0 || value == MAX_VALUE)
-	{
-	    try 
-	    {
-	    	throw value;
-	    }
-	    catch (long value)
-	    {
-	    	cout << "Can only accept values in the range 0.. " <<  MAX_VALUE - 1 << endl;
-	    }
-	}
+  if (value < 0 || value == MAX_VALUE)
+  {
+    try 
+    {
+      throw value;
+    }
+    catch (long value)
+    {
+      cout << "Can only accept values in the range 0.. " <<  MAX_VALUE - 1 << endl;
+    }  
+}
 	
-    // Rebuild if the value is too large for the current tree height
-    if (value >= capacity)
-	{
-	   	rebuildToCapacity(highestOneBit(value) << 1);   
-	   	/* 
-	   	highestOneBit() returns a long value with at most a single one-bit, in the position 
-   		 of the highest-order ("leftmost") one-bit in the specified int value. 
-   	 	For example, value = 220
-		Binary = 11011100
-		Highest one bit = 128 
-		*/
-	
-	}	
-   long leaf = value2leaf(value);
-   if (node2count.find(leaf) == node2count.end())
-     node2count.insert(std::make_pair<long, long>(leaf, get(leaf)+1));
-   else
-   {
-     long hold = node2count[leaf];
-     node2count[leaf] = hold + 1;
-   }
-   /*
+  // Rebuild if the value is too large for the current tree height
+  if (value >= capacity)
+    rebuildToCapacity(highestOneBit(value) << 1);   
+    /* 
+      highestOneBit() returns a long value with at most a single one-bit, in the position 
+      of the highest-order ("leftmost") one-bit in the specified int value. 
+      For example, value = 220
+      Binary = 11011100
+      Highest one bit = 128 
+    */
+		
+ long leaf = value2leaf(value);
+ if (node2count.find(leaf) == node2count.end())
+   node2count.insert(std::make_pair<long, long>(leaf, get(leaf)+1));
+ else
+   node2count[leaf] += 1;
+ /*
    Java syntax: node2count.put(leaf, get(leaf) + 1);
    value put(Key k, Value v): Inserts key value mapping into the map. 
    The get(Object key) method is used to return the value to which the specified key is mapped, 
    or null if this map contains no mapping for the key. 
-   */
+ */
    
-   size++;
-   /*
-          Always compress at the inserted node, and recompress fully
-          if the tree becomes too large.
-          This is one sensible strategy which both is fast and keeps
-          the tree reasonably small (within the theoretical bound of 3k nodes)
-   */
-   compressUpward(leaf);
-   if (node2count.size() > 3 * k) 
-      compressFully();
+ size++;
+ /*
+   Always compress at the inserted node, and recompress fully
+   if the tree becomes too large.
+   This is one sensible strategy which both is fast and keeps
+   the tree reasonably small (within the theoretical bound of 3k nodes)
+ */
+ compressUpward(leaf);
+ if (node2count.size() > 3 * k) 
+   compressFully();
 }
 
 long QDigest::get(long node)
 {
   /*
-  		Given the index of the index, then search the container/map.
-  		Return the corresponding key's value if found, 0 otherwise.
-  		how find() works: ttp://www.cplusplus.com/reference/unordered_map/unordered_map/find/
+    Given the index of the index, then search the container/map.
+    Return the corresponding key's value if found, 0 otherwise.
+    how find() works: ttp://www.cplusplus.com/reference/unordered_map/unordered_map/find/
   */ 
   std::unordered_map<long, long>::const_iterator got = node2count.find(node); 
   return (got == node2count.end()) ? 0 : got->second;
@@ -154,15 +149,13 @@ long QDigest::get(long node)
 
 void QDigest::compressFully()
 {
-    std::vector<long> keys;
-	keys.reserve(node2count.size());
-	for (std::unordered_map<long, long>::const_iterator i = node2count.begin(); i != node2count.end(); i++)
-   		 keys.push_back(i->first);
+  std::vector<long> keys;
+  keys.reserve(node2count.size());
+  for (std::unordered_map<long, long>::const_iterator i = node2count.begin(); i != node2count.end(); i++)
+    keys.push_back(i->first);
 
-    for (std::vector<long>::const_iterator i = keys.begin(); i != keys.end(); i++)
-    {
-   		compressDownward(*i);   // java syntax
-   	}
+  for (std::vector<long>::const_iterator i = keys.begin(); i != keys.end(); i++)
+    compressDownward(*i);   // java syntax
 }
 
 void QDigest::compressUpward(long node)
@@ -176,20 +169,21 @@ void QDigest::compressUpward(long node)
     long atSibling = get(sibling(node));
     if (atNode + atSibling > threshold)
       break;
-    long atParent = get(parent(node));
+    long p = parent(node);
+    long atParent = get(p);
     if (atNode + atSibling + atParent > threshold)
       break;
-    if (node2count.find(parent(node)) == node2count.end())
-	node2count.insert(std::make_pair<long, long>(parent(node), atParent + atNode + atSibling));
+    if (node2count.find(p) == node2count.end())
+	node2count.insert(std::make_pair<long, long>(p, atParent + atNode + atSibling));
     else
       {
-	long hold = node2count[parent(node)];
-	node2count[parent(node)] = hold + 1;
+	long hold = node2count[p];
+	node2count[p] += 1;
       }
     node2count.erase(node);
     if (atSibling > 0)
       node2count.erase(sibling(node));
-    node = parent(node);
+    node = p;
     atNode = atParent + atNode + atSibling;
   }
 }
@@ -199,38 +193,36 @@ void QDigest::compressUpward(long node)
   */
 void QDigest::compressDownward(long seedNode)
 {
-	double threshold = std :: floor(size/k); // java syntax
-	// 2nd property check same as above but shorter and slower (and invoked rarely)
+  double threshold = std :: floor(size/k); // java syntax
+  // 2nd property check same as above but shorter and slower (and invoked rarely)
 	
-	std::queue<long, std::list<long> > q;
-	q.push(seedNode);
-	while(!q.empty())
-	{
-		long node = q.front();
-		q.pop();
-		long atNode = get(node);
-		long atSibling = get(sibling(node));
-		if (atNode == 0 && atSibling == 0)
-		{
-			continue;
-		}
-		long atParent = get(parent(node));
-		if (atParent + atNode + atSibling > threshold)
-		{
-			continue;
-		}
-		// fix insert here as well?
-		node2count.insert(std::make_pair<long, long>(parent(node), atParent + atNode + atSibling));
-		node2count.erase(node);
-		node2count.erase(sibling(node));
-		// Now P2 could have vanished at the node's and sibling's subtrees since they decreased.
-		if (!isLeaf(node))
-		    {
-			q.push(leftChild(node));
-			q.push(leftChild(sibling(node)));
-		    }
-		
-	}
+  std::queue<long, std::list<long> > q;
+  q.push(seedNode);
+  while(!q.empty())
+  {
+    long node = q.front();
+    q.pop();
+    long atNode = get(node);
+    long atSibling = get(sibling(node));
+    if (atNode == 0 && atSibling == 0)
+      continue;
+    long p = parent(node);
+    long atParent = get(p);
+    if (atParent + atNode + atSibling > threshold)
+       continue;
+    if (node2count.find(p) == node2count.end())
+      node2count.insert(std::make_pair<long, long>(p, atParent + atNode + atSibling));
+    else
+      node2count[p] += 1;
+    node2count.erase(node);
+    node2count.erase(sibling(node));
+    // Now P2 could have vanished at the node's and sibling's subtrees since they decreased.
+    if (!isLeaf(node))
+    {
+      q.push(leftChild(node));
+      q.push(leftChild(sibling(node)));
+    }
+  }
 }	
 
 long QDigest::rangeLeft(long id)
@@ -249,21 +241,18 @@ long QDigest::rangeRight(long id)
 
 int QDigest::highestOneBit(long value)
 {
-    if (!value)
-    	return 0;
-
-    int ret = 1;
-
-    while (value >>= 1)
-    	ret <<= 1;
-
-    return ret;
+  if (!value)
+    return 0;
+  int ret = 1;
+  while (value >>= 1)
+    ret <<= 1;
+  return ret;
 }
 
 void QDigest::rebuildToCapacity(long newCapacity) // check accuracy
 {
-	 std::unordered_map<long, long> newNode2count;
-    /*
+  std::unordered_map<long, long> newNode2count;
+  /*
      rebuild to newLogCapacity.
      This means that our current tree becomes a leftmost subtree
      of the new tree.
@@ -274,36 +263,27 @@ void QDigest::rebuildToCapacity(long newCapacity) // check accuracy
      nodes 4..7 => 32..35 (+= 28 = 2^2*(2^3-1))
      This is easy to see if you draw it on paper.
      Process the keys by "layers" in the original tree.
-    */
-    long scaleR = newCapacity / capacity - 1;
-    
-    std::vector<long> keys;
-    keys.reserve(node2count.size());
+  */
+  long scaleR = newCapacity / capacity - 1;  
+  std::vector<long> keys;
+  keys.reserve(node2count.size());
 	
-	for (std::unordered_map<long, long>::const_iterator i = node2count.begin(); i != node2count.end(); i++)
-   		 keys.push_back(i->first);
-	/* 	Alternative of for loop:
-	for(auto k : node2count) 
-   	 	keys.push_back(k.first);
-	*/
+  for (std::unordered_map<long, long>::const_iterator i = node2count.begin(); i != node2count.end(); i++)
+    keys.push_back(i->first);
 	
-	std::sort(keys.begin(), keys.end());
+  std::sort(keys.begin(), keys.end());
     //the place i find how to rewrite
     //http://stackoverflow.com/questions/8483985/obtaining-list-of-keys-and-values-from-unordered-map#comment10496288_8484055
     
-    long scaleL = 1;
-    for (std::vector<long>::const_iterator i = keys.begin(); i != keys.end(); i++)
-	{
-		while (scaleL <= *i / 2) // see the use of iterator: 
-								 // http://www.cplusplus.com/reference/vector/vector/begin/
-		{
-			scaleL <<= 1;
-		 }
-		newNode2count.insert(std::make_pair<long, long>(*i + scaleL * scaleR, get(*i))); 
-		//don't forget to take off the above comment.
-	 }
-    node2count = newNode2count;
-    capacity = newCapacity;
-    compressFully();
+  long scaleL = 1;
+  for (std::vector<long>::const_iterator i = keys.begin(); i != keys.end(); i++)
+  {
+    while (scaleL <= *i / 2) // see the use of iterator: // http://www.cplusplus.com/reference/vector/vector/begin/
+      scaleL <<= 1;
+    newNode2count.insert(std::make_pair<long, long>(*i + scaleL * scaleR, get(*i))); 
+   }
+  node2count = newNode2count;
+  capacity = newCapacity;
+  compressFully();
 }
 
