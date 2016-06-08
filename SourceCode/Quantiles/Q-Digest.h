3b60a4fd81d8e3c442ece2bf6ad8a8df688c76c4
EmilyFarrow <farrow_e1@denison.edu>
2016-06-08 11:51:40 -0400

    organized functions

--- SourceCode/Quantiles/Q-Digest.h
+++ SourceCode/Quantiles/Q-Digest.h
@@ -26,31 +26,31 @@ class QDigest : public QuantileSketch
   double getQuantile(double f);
 
  private:
-  long value2leaf(long x) { return capacity + x; };
-  long leaf2value(long id) { return id - capacity; };
-  bool isRoot(long id) { return id == 1; }; // Check if the input index is root or not. (Root index is 1)
-  bool isLeaf(long id) { return id >= capacity; }; // Check if the input index is leaf or not. 
-  long sibling(long id) { return (id % 2 == 0) ? (id + 1) : (id - 1); };// Return the index of its sibling.
-  long parent(long id) { return id / 2; }; // Return the index of its parent.
-  long leftChild(long id) { return 2 * id; };// Return the index of its left child.
-  long rightChild(long id) { return 2 * id + 1; }; // Return the index of its right child.
-  
-  long rangeLeft(long id);
-  long rangeRight(long id);
-  long clamp(long x);
-  long get(long node);
-  int highestOneBit(long value); 
-  
   void offer(long value);
   void rebuildToCapacity(long newCapacity);  	
   void compressUpward(long node);
   void compressFully();
   void compressDownward(long seedNode);
-  void copy(const QDigest& q);
 
   std::vector<long*> toAscRanges();
   void delete_ranges(std::vector<long*> ranges);
   static bool compare_ranges(long a[3], long b[3]);
+  void copy(const QDigest& q);
+
+  long rangeLeft(long id);
+  long rangeRight(long id);
+  long clamp(long x);
+  long get(long node);
+  int highestOneBit(long value);
+
+  long value2leaf(long x) { return capacity + x; };
+  long leaf2value(long id) { return id - capacity; };
+  bool isRoot(long id) { return id == 1; }; // Check if the input index is root or not. (Root index is 1)
+  bool isLeaf(long id) { return id >= capacity; }; // Check if the input index is leaf or not.
+  long sibling(long id) { return (id % 2 == 0) ? (id + 1) : (id - 1); };// Return the index of its sibling.
+  long parent(long id) { return id / 2; }; // Return the index of its parent.
+  long leftChild(long id) { return 2 * id; };// Return the index of its left child.
+  long rightChild(long id) { return 2 * id + 1; }; // Return the index of its right child. 
 
   std::unordered_map<long, long> node2count; //= new std::unordered_map<long, long>(), Map<key, value>
   long size, capacity, min, max;
