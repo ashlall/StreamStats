bf5d73b08edc1216636fa2cfc66da2a297afdd4a
Drown1994 <larry812junbo@gmail.com>
2016-06-09 15:46:03 -0400

    add corner cases, seed genertor cases

--- Test/QDigest_test_Double.cpp
+++ Test/QDigest_test_Double.cpp
@@ -9,29 +9,79 @@ using namespace std;
 
 void test_qdigetDouble()
 {
+
+	int k = 5000; //make an arbitrarily large k for testing 	
+	//Compress factor: usually the choose of k ranges from 0.3n to 0.5n in order to get the best query results
+	
 	int n = 1000;
-	int k = 0.5 * n; //compress factor 
-					// usually the choose of k ranges from 0.3n to 0.5n in order to get the best query results
+
 					
-	long quantA,quantB,quantC;
-	bool condA,condB,condC;
+	long quantA,quantB,quantC,quantD,quantE,quantF;
+	bool condA,condB,condC,condD,condE,condF;
     double upper,lower;
+    
+    int random_fix1 = rand()% RAND_MAX; //used to test corner case where n = 1
+    int random_fix2 = rand()% RAND_MAX;
+    int random_fix3 = rand()% RAND_MAX;
     
 	QDigest a(k);
 	QDigest b(k);
-	//QDigest c(k);
+	QDigest c(k);
+	QDigest d(k);
+    QDigest e(k);
+	QDigest f(k);
 	
-	for (double i = 0; i <= n; i = i + 0.1)
+	for (double i = 0; i <= n; i = i + 0.1) //inserting in increasing order
     {	
-  		a.insert(i); //inserting in increasing order
+  		a.insert(i); 
     }
     
-	for (double j = n; j >= 0; j = j - 0.1)
+	for (double j = n; j >= 0; j = j - 0.1) //inserting in decreasing order
     {	
-  		b.insert(j); //inserting in increasing order
+  		b.insert(j); 
     }
   	
-	//random floating number from MIN ~ MAX are needed. Use seed().. !!!!!
+  	
+  	for (int m = 1; m <= 3; m++) 
+  	{
+  		 srand(m); //use the random number generator seeds 1 - 5.
+  		 if (m == 1)
+  		 {
+  			 for (int i = 0; i <= n; i++)
+   			 {
+  				double rand_num=rand()%n + 0.5;
+  				c.insert(rand_num); 
+  				//inserting float number from 0 to 1000
+  			 }  
+  		}
+  		
+  		else if(m == 2)
+  		{
+  			  for (int i = 0; i <= n; i++)
+   			 {
+  				double rand_num=rand()%n + 0.5;
+  				d.insert(rand_num); 
+  				//inserting float number from 0 to 1000
+  			 }  
+  		}	
+  		
+  		else 
+  		{	
+  			  for (int i = 0; i <= n; i++)
+   			 {
+  				double rand_num=rand()%n + 0.5;
+  				e.insert(rand_num); 
+  				//inserting float number from 0 to 1000
+  			 }  
+  		}
+  	}
+  	
+  
+  	f.insert(random_fix1); //test corner case where n = 1
+  	
+  	
+	//random floating number from MIN ~ MAX are needed !!!!!
+	
 
     for (float i=1;i<=100;i++)
     {
@@ -39,10 +89,13 @@ void test_qdigetDouble()
   	  
   	  quantA=a.getQuantile(interval); //getting quantile for each probability interval
   	  quantB=b.getQuantile(interval);//getting quantile for each probability interval
-  	  //quantC=c.getQuantile(interval);//getting quantile for each probability interval
+  	  quantC=c.getQuantile(interval);//getting quantile for each probability interval
+  	  quantD=d.getQuantile(interval);
+  	  quantE=e.getQuantile(interval);
+  	  quantF=f.getQuantile(interval);
   	  
-  	  lower=(interval-0.02)*n;
-  	  upper=(interval+0.02)*n;
+  	  lower=(interval-0.04)*n;
+  	  upper=(interval+0.04)*n;
   	  
   	  condA= ((lower<=quantA) && (upper>=quantA));
   	  //checking if quantile is within error range
@@ -50,19 +103,27 @@ void test_qdigetDouble()
   	  condB= ((lower<=quantB) && (upper >=quantB));
   	  //checking if quantile is within error range
   	  
-  	 //condC= ((lower<=quantC) && (upper>=quantC));
+  	  condC= ((lower<=quantC) && (upper>=quantC));
+  	  condD= ((lower<=quantD) && (upper>=quantD));
+  	  condE= ((lower<=quantE) && (upper>=quantE));
   	  //checking if quantile is within error range
+  	  
+  	  
+  	  condF= (random_fix1 = quantF);
+  	  //checking if the condition holds in the corner case where stream size = 1
   	  
   	  assert(condA==true);
   	  assert(condB==true);
-  	  //assert(condC==true);
+  	  assert(condC==true);
+  	  assert(condD==true);
+  	  assert(condE==true);
+  	  assert(condF==true);
   } 
- 
-	  /*
+
+	/*  
 		QDigestDouble a(500);
-		for(double i=1; i<=1000; i = i + 0.3)
+		for(double i=1; i<=1000; i = i + 0.5)
 		{
-			cout << "i: " << i << endl;
 			a.insert(i);
 		}
 
@@ -72,7 +133,7 @@ void test_qdigetDouble()
 		cout << "0.4: "<<a.getQuantile(0.4) << endl;
 		cout << "0.8: "<<a.getQuantile(0.8) << endl;
 		cout << "hi" <<endl; 
-*/
+   */
 }
 
 
