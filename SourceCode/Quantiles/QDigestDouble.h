51969f8bc12cbf2f3296f1088725a01709057076
EmilyFarrow <farrow_e1@denison.edu>
2016-06-09 09:44:02 -0400

    gets rid of unnecessary lines in QDigestDouble.h

--- SourceCode/Quantiles/QDigestDouble.h
+++ SourceCode/Quantiles/QDigestDouble.h
@@ -1,5 +1,4 @@
 // QDigestDouble.h, sub class of Q-Digest that takes doubles and converts them to longs
-// ****not yet compiled or tested****
 
 #include "Q-Digest.h"
 
@@ -11,31 +10,16 @@ class QDigestDouble : public QDigest
  public:
  QDigestDouble(double compression) : QDigest(compression) { 
     precision = 1000000000.0; 
-    range = 1000000000.0;
-    min = range;
-    max = -range; };
+    range = 1000000000.0; };
   QDigestDouble(const QDigestDouble& q) : QDigest(q) {};
   
-  void insert(double x) { 
-    if (x < min)
-      min = x;
-    if (x > max)
-      max = x;
-    QDigest::insert(doubleToLong(x)); };
-  double getQuantile(double f) { return clamp(longToDouble(QDigest::getQuantile(f))); };
+  void insert(double x) { QDigest::insert(doubleToLong(x)); };
+  double getQuantile(double f) { return longToDouble(QDigest::getQuantile(f)); };
 
  private:
   long doubleToLong(double d) { return (long)(((d + range)/(2 * range)) * precision); };
   double longToDouble(long l) { return (((double)l)/precision) * 2 * range - range; };
-  double clamp(double value){
-    if (value < min)
-      return min;
-    if (value > max)
-      return max;
-    return value; };
-  double precision, min, max, range;
+  double precision, range;
 };
-
-#include "QDigestDouble.cpp"
 
 #endif
