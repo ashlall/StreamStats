41b15e2fbf9d9676edaecc1586e361a128542444
Farhan Zaki <zaki_f1@denison.edu>
2016-06-13 15:11:12 -0400

    converted to class

--- SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_1st.h
+++ SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_1st.h
@@ -1,1 +1,50 @@
+// Chi_Squared_1st.h
 
+#ifndef CHI_SQUARED_1ST_H
+#define CHI_SQUARED_1ST_H
+
+#include "../Quantiles/GK.h"
+#include "../Quantiles/ReservoirSampling.h"
+#include "../Quantiles/Q-Digest.h"
+#include "../Quantiles/CMSQuantile.h"
+#include <iostream>
+#include <stdlib.h> 
+#include<math.h>
+
+
+class ChiSquare
+{
+public:
+	ChiSquare();
+	ChiSquare(int n,int k,double m); //m is the percent of memory
+	ChiSquare(int q,int n,int k,double m);
+	/* q=1: GreenWaldKhanna Sketch
+	   q=2: Q-Digest Sketch
+	   q=3: Count Min Sketch
+	   q=4 Reservoir Sampling
+	*/
+	
+	~ChiSquare();
+	void insert(double val);
+	double calculate_statistic();
+	  
+	
+private:
+	double chi_squared;
+	double memory;
+	int N;
+	int K;
+	int Q;
+	
+	GK* quantile_GK;
+	QDigest* quantile_QD;
+	ReservoirSampling* quantile_RS;
+	CMS* quantile_CMS;
+	
+	double inverse_cmf();
+	
+	
+};
+
+#include"Chi_Squared_1st.cpp"
+#endif
