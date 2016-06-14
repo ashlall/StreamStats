8e26875d7b04877bcc289d4cfa5f1e68e7f3bd39
Drown1994 <larry812junbo@gmail.com>
2016-06-14 15:28:50 -0400

    test of finding normal inverse CDF

--- Test/Chi_Square_test.cpp
+++ Test/Chi_Square_test.cpp
@@ -1,14 +1,76 @@
-// Unit tests for One-Sample Chi-square test
+// Unit tests for One-Sample Chi-square & Two sample Chi-square.
 
 #include<cassert>
 #include "../SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_1st.h"
 #include <iostream>
+#include<iomanip> 
 
 using namespace std;
 
 void test_chi_square_1st() //One-sample test
 {
+	bool condA;
+	
+    double p[] =	// used for testing the accuracy of the function NormalCDFInverse_pub();
+    {
+        0.0000001,
+        0.00001,
+        0.001,
+        0.05,
+        0.15,
+        0.25,
+        0.35,
+        0.45,
+        0.55,
+        0.65,
+        0.75,
+        0.85,
+        0.95,
+        0.999,
+        0.99999,
+        0.9999999
+    };
+    
+    double exact[] = // used for testing the accuracy of the function NormalCDFInverse_pub();
+    {
+        -5.199337582187471,
+        -4.264890793922602,
+        -3.090232306167813,
+        -1.6448536269514729,
+        -1.0364333894937896,
+        -0.6744897501960817,
+        -0.38532046640756773,
+        -0.12566134685507402,
+        0.12566134685507402,
+        0.38532046640756773,
+        0.6744897501960817,
+        1.0364333894937896,
+        1.6448536269514729,
+        3.090232306167813,
+        4.264890793922602,
+        5.199337582187471
+    };
 
+	ChiSquare a(3000);
+	double accurate, estimate;
+	
+	for (int i = 0; i <=15; i++)
+	{
+		for (int mean = 0; mean <= 5000; mean = mean + 5)
+		{
+			for (int SD = 1; SD <= 100; SD++)
+			{
+				cout << setprecision(3);
+				accurate = exact[i]*SD+mean;
+				estimate = a.NormalCDFInverse_pub(p[i], mean, SD);
+				//cout << "accurate: " << accurate <<" estimate: " << estimate <<endl; 
+				condA = ((estimate<= accurate+0.05) && (estimate >= accurate - 0.05));
+  	  			assert(condA==true);
+			}	
+		}
+	}
+
+	
 }
 
 
@@ -19,8 +81,9 @@ void test_chi_square_2nd() //Two-sample test
 }
 
 
-int main
+int main()
 {
 	test_chi_square_1st();
+	//test_chi_square_2nd();
 	return 0;
 }
