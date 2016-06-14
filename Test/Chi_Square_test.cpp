4161630e967614fc102fd0446848514b80bdf55d
Drown1994 <larry812junbo@gmail.com>
2016-06-13 21:10:24 -0400

    chi_square test

--- Test/Chi_Square_test.cpp
+++ Test/Chi_Square_test.cpp
@@ -1,1 +1,26 @@
+// Unit tests for One-Sample Chi-square test
 
+#include<cassert>
+#include "../SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_1st.h"
+#include <iostream>
+
+using namespace std;
+
+void test_chi_square_1st() //One-sample test
+{
+
+}
+
+
+
+void test_chi_square_2nd() //Two-sample test
+{
+
+}
+
+
+int main
+{
+	test_chi_square_1st();
+	return 0;
+}
