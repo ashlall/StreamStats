7a65cb6acce98ba1183d834dd6286afe68880631
Drown1994 <larry812junbo@gmail.com>
2016-06-16 16:19:18 -0400

    fix error

--- Test/Chi_Square_test.cpp
+++ Test/Chi_Square_test.cpp
@@ -9,6 +9,74 @@
 
 using namespace std;
 
+void test_NormalCDFInverse()
+{
+    /*
+	Test function NormalCDFInverse_pub(double p, double mean, double SD); PASSED
+	
+    double p[] =	// used for testing the accuracy of the function NormalCDFInverse_pub();
+    {
+        0.0000001,
+        0.00001,
+        0.001,
+        0.05,
+        0.15,
+        0.25,
+        0.35,
+        0.45,
+        0.55,
+        0.65,
+        0.75,
+        0.85,
+        0.95,
+        0.999,
+        0.99999,
+        0.9999999
+    };
+    
+    double exact[] = // used for testing the accuracy of the function NormalCDFInverse_pub();
+    {
+        -5.199337582187471,
+        -4.264890793922602,
+        -3.090232306167813,
+        -1.6448536269514729,
+        -1.0364333894937896,
+        -0.6744897501960817,
+        -0.38532046640756773,
+        -0.12566134685507402,
+        0.12566134685507402,
+        0.38532046640756773,
+        0.6744897501960817,
+        1.0364333894937896,
+        1.6448536269514729,
+        3.090232306167813,
+        4.264890793922602,
+        5.199337582187471
+    };
+
+	ChiSquare a(3000);
+	bool condA;
+	double accurate, estimate;
+	
+	for (int i = 0; i <=15; i++)
+	{
+		for (int mean = 0; mean <= 5000; mean = mean + 5)
+		{
+			for (int SD = 1; SD <= 100; SD++)
+			{
+				cout << setprecision(3);
+				accurate = exact[i]*SD+mean;
+				estimate = a.NormalCDFInverse_pub(p[i], mean, SD);
+				//cout << "accurate: " << accurate <<" estimate: " << estimate <<endl; 
+				condA = ((estimate<= accurate+0.05) && (estimate >= accurate - 0.05));
+  	  			assert(condA==true);
+			}	
+		}
+	}
+	
+ */
+ }
+ 
 void test_chi_square_1st() //One-sample test
 {
 	ChiSquare b(100000); //ChiSquare b(memory, sketch method);
@@ -49,12 +117,12 @@ void test_chi_square_1st() //One-sample test
     }
     cout<<chiActual<<" actual"<<endl;
   	cout << "chi: " << chi << endl;
-  	delete items;
+  	delete []items;
 }
 
 
 /*
-void test_chi_square_2nd() //Two-sample test
+void test_chi_square_2nd() //Two-sampwle test
 {
 	ChiSquare2 c(100); 
 	int stream_size = 1000;
@@ -86,5 +154,6 @@ int main()
 {
 	test_chi_square_1st();
 	//test_chi_square_2nd();
+	//test_NormalCDFInverse();
 	return 0;
 }
