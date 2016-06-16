8b5fb854c833e52430a41f49e4dfc363a625bf37
Farhan Zaki <zaki_f1@denison.edu>
2016-06-16 10:25:07 -0400

    fixed bugs

--- SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_2nd.cpp
+++ SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_2nd.cpp
@@ -1,6 +1,6 @@
 //Two Sample Chi-Squared Test
 #include<cassert>
-#include "../Quantiles/GK.h"
+//#include "../Quantiles/GK.h"
 #include <iostream>
 #include <stdlib.h> 
 #include<math.h> 
@@ -10,28 +10,30 @@ ChiSquare2::ChiSquare2(double m)
 {
 	Q=0;
 	chi_squared=0;
-	quantile1=new GK((int)(m/3));
-	quantile2=new GK((int)(m/3));
+	quantile1=new GK((int)(m));
+	quantile2=new GK((int)(m));
 }
 
 ChiSquare2::ChiSquare2(double m,int q)
 {
 	Q=q;
-	chi_squared=0;
-	memory= m;
 	switch(Q)
 	{
-	case 1: quantile1=new GK((int)(memory/3));
-		quantile2=new GK((int)(memory/3));
+	case 1: memory=m;
+		quantile1=new GK((int)(memory));
+		quantile2=new GK((int)(memory));
 		break;
-	case 2:// quantile1=new QDigestDouble(memory);
-		// quantile2=new QDigestDouble(memory);
+	case 2:memory=m;
+		quantile1=new QDigestDouble(memory);
+		quantile2=new QDigestDouble(memory);*/
 		break;
-	case 3: //quantile1=new ReservoirSampling((int)memory);
-		//quantile2=new ReservoirSampling((int)memory)
+	case 3: memory=m;
+		quantile1=new ReservoirSampling((int)memory);
+		quantile2=new ReservoirSampling((int)memory)*/
 		break;
-	case 4:// quantile1=new CMS((int)memory);
-		// quantile2=new CMS((int)memory);
+	case 4: /*memory=m;
+		quantile1=new CMS((int)memory);
+		quantile2=new CMS((int)memory);*/
 		break;
 	default:
 		cout<<" Incorrect Case. Valid inputs lie between 1 and 4"<<endl;
@@ -63,11 +65,11 @@ double ChiSquare2::calculate_statistic(int k)
 	double S=N/K;
 	for (double i=1;i<=K;i++)
 	{
-		double l= (*f)((i-1)/K);
-		double u= (*f)(i/K);
+		double l= quantile1->getQuantile((i-1)/K);
+		double u= quantile1->getQuantile(i/K);
 		double iA,iB;
-		iA=quantile2->reverseQuantile(l,100);
-		iB=quantile2->reverseQuantile(u,100);
+		iA=(quantile2->reverseQuantile(l,memory))/memory;
+		iB=(quantile2->reverseQuantile(u,memory))/memory;
 		double R=M*(iB-iA);
 		
 		chi_squared=chi_squared+ ((((S*sqrt(M/N)-R*sqrt(N/M))*(S*sqrt(M/N)-R*sqrt(N/M)))/ (S+R)));
