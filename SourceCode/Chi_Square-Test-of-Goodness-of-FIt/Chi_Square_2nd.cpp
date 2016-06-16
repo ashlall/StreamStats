9e1b10a58058272ead5f730fbf9a84e60d03fd4b
Farhan Zaki <zaki_f1@denison.edu>
2016-06-16 14:21:37 -0400

    f

--- SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_2nd.cpp
+++ SourceCode/Chi_Square-Test-of-Goodness-of-FIt/Chi_Square_2nd.cpp
@@ -10,7 +10,7 @@ ChiSquare2::ChiSquare2(double m)
 	chi_squared=0;
 	memory=m;
 	quantile1=new GK(memory);
-	quantile2=new GK((memory);
+	quantile2=new GK(memory);
 }
 
 ChiSquare2::ChiSquare2(double m,int q)
@@ -19,8 +19,8 @@ ChiSquare2::ChiSquare2(double m,int q)
 	switch(Q)
 	{
 	case 1: memory=m;
-		quantile1=new GK((int)(memory));
-		quantile2=new GK((int)(memory));
+		quantile1=new GK((int)memory);
+		quantile2=new GK((int)memory);
 		break;
 	case 2:/*memory=m;
 		quantile1=new QDigestDouble(memory);
