e347adee993e0c1a5a76940041e44242fb77f541
Farhan Zaki <zaki_f1@denison.edu>
2016-06-17 13:58:57 -0400

    changed variable names

--- SourceCode/Quantiles/GK.cpp
+++ SourceCode/Quantiles/GK.cpp
@@ -10,8 +10,8 @@ Post-Condition: A Dynamically alloacted array of maxTuples.
 */
 GK::GK(int maxTuples)
 {
-	numTuples=0;
-	numObservations=0;
+	num_tuples=0;
+	num_observations=0;
 	max=maxTuples;
 	summary=new Tuple*[maxTuples];
 	stream_size =0;
@@ -35,45 +35,45 @@ Post-Condition: A tuple holding val as its value is inserted into the summary.
 void GK::insert(double val)
 {
 	stream_size +=1; 
-	int index= findIndex(val);
+	int index= find_index(val);
 	if(summary[index]==NULL)
 	{	
-		insertTuple(new Tuple(val),index); 
+		insert_tuple(new Tuple(val),index); 
 	}
 	else
 	{
 		double store=summary[index]->g+summary[index]->delta-1;
-		insertTuple(new Tuple(val,1.0,store),index); 
+		insert_tuple(new Tuple(val,1.0,store),index); 
 	}
-	if(numTuples==max)
-		deleteMerge();
-	numObservations++;
+	if(num_tuples==max)
+		delete_merge();
+	num_observations++;
 }
 /*
 Pre-Condition: The Tuple to be inserted and the index in summary at
 which at has to be inserted.
 Post-Condition: The Tuple is inserted is inserted at index.
 */
-void GK::insertTuple(Tuple* t,int index)
+void GK::insert_tuple(Tuple* t,int index)
 {	
-	int destpos=numTuples;
-	int srcpos=numTuples-1;
-	for(int i=0;i<numTuples-index;i++)
+	int destpos=num_tuples;
+	int srcpos=num_tuples-1;
+	for(int i=0;i<num_tuples-index;i++)
 	{	
 		summary[destpos]=summary[srcpos];
 		srcpos--;
 		destpos--;
 	}
 	summary[index]=t;
-	numTuples++;
+	num_tuples++;
 }
 /*
 Pre-Condition: A double value
 Post-Condition: Finds the index of a tuple whose value is greater than value
 */
-int GK::findIndex(double val)
+int GK::find_index(double val)
 {
-	int high= numTuples;int low=-1,mid;
+	int high= num_tuples;int low=-1,mid;
 	while(high-low >1)
 	{
 		mid=(high+low)/2;
@@ -88,11 +88,11 @@ int GK::findIndex(double val)
 Pre-Condition:None.
 Post-Condition: Does a merge and deletes the tuple that is least full.
 */
-void GK::deleteMerge()
+void GK::delete_merge()
 {	
 	int leastIndex = 0;
 	long leastFull= LLONG_MAX;
-        for (int i = 1; i <numTuples - 1; i++) 
+        for (int i = 1; i <num_tuples - 1; i++) 
         {
             long f = summary[i]->g + summary[i + 1]->g+summary[i + 1]->delta;   
             if ((summary[i]->delta >=summary[i + 1]->delta)
@@ -103,20 +103,20 @@ void GK::deleteMerge()
             }
         }
         if (leastIndex > 0) 
-            deleteTuple(leastIndex);
+            delete_tuple(leastIndex);
 }
 /*
 Pre-Condition: the index of the tuple to be deleted.
 Post-Condition: the tuple at the index is deleted.
 */
-void GK::deleteTuple(int index)
+void GK::delete_tuple(int index)
 {	
 	int srcpos=index+2;
 	int destpos=index+1;
 	summary[index] = new Tuple(summary[index + 1]->v,
                 summary[index]->g + summary[index + 1]->g,
                 summary[index + 1]->delta);
-	for (int i=0;i<numTuples-index-2;i++)
+	for (int i=0;i<num_tuples-index-2;i++)
 	{	
 		summary[destpos]->v=summary[srcpos]->v;
 		summary[destpos]->g=summary[srcpos]->g;
@@ -124,8 +124,8 @@ void GK::deleteTuple(int index)
 		srcpos++;
 		destpos++;
 	}
-	summary[numTuples-1]=NULL;
-	numTuples--;
+	summary[num_tuples-1]=NULL;
+	num_tuples--;
 }
 /*
 Pre-Condition: The quantile interval.
@@ -133,15 +133,15 @@ Post-Condition: The quantile estimate.
 */
 double GK::getQuantile(double quant)
 {
-	long rank= (long) ceil(quant* numObservations);
+	long rank= (long) ceil(quant* num_observations);
 	long current_rank=0;
-	for (int i = 0; i < numTuples - 1; i++)
+	for (int i = 0; i < num_tuples - 1; i++)
 	{
 		current_rank +=summary[i]->g;
 		if ((current_rank +summary[i + 1]->g) > rank)
             	    return summary[i]->v;
         }
-        return summary[numTuples - 1]->v;
+        return summary[num_tuples - 1]->v;
 }
 
 
