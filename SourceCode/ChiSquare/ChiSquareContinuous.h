3e06ef84773d83e3a4092874d72dedc704e2b026
EmilyFarrow <farrow_e1@denison.edu>
2016-06-20 14:58:27 -0400

    adds comments

--- SourceCode/ChiSquare/ChiSquareContinuous.h
+++ SourceCode/ChiSquare/ChiSquareContinuous.h
@@ -1,4 +1,5 @@
 // Chi_Squared_1st.h
+// Inserts a stream of data into a quantile-epsilon sketch chosen by the user and calculates the Chi-Squared statistic related to the normal distribution, a given distribution, or another stream of data
 
 #ifndef CHI_SQUARED_CONTINUOUS_H
 #define CHI_SQUARED_CONTINUOUS_H
@@ -12,6 +13,7 @@
 #include<math.h>
 #include<iomanip> // Parametric manipulators. Used to set precision. 
 
+// Inserts continuous data into a quantile sketch and computes the chi-squared statistic 
 class ChiSquareContinuous
 {
 public:
@@ -21,8 +23,7 @@ public:
 	   q=2: Q-Digest Sketch
 	   q=3: Count Min Sketch
 	   q=4 Reservoir Sampling
-	*/
-	
+	*/	
 	~ChiSquareContinuous();
   	
 	void insert(double val);
@@ -41,8 +42,7 @@ private:
 	QuantileSketch* quantile_sketch;
 	
 	double rational_approximation(double t);
-	double normal_cdf_inverse(double p, double mean, double SD);
-	
+	double normal_cdf_inverse(double p, double mean, double SD);	
 };
 
 class IndexError {};
