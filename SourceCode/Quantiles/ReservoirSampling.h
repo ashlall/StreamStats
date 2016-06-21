ae834cf8c6fa50862f3716613aebf1a01a446e50
EmilyFarrow <farrow_e1@denison.edu>
2016-06-21 09:33:32 -0400

    adds comments to ReservoirSampling.h

--- SourceCode/Quantiles/ReservoirSampling.h
+++ SourceCode/Quantiles/ReservoirSampling.h
@@ -9,22 +9,25 @@ using namespace std;
 #ifndef RESERVOIR_SAMPLING
 #define RESERVOIR_SAMPLING
 
+// Holds a sample of the inserted data in an array and allows for queries to
+// find the quantile of a value
 class ReservoirSampling: public QuantileSketch
 {
  public:
   ReservoirSampling(int items);
-  ReservoirSampling(const ReservoirSampling& r);
-  ReservoirSampling& operator=(const ReservoirSampling& r);
+  ReservoirSampling(const ReservoirSampling& other);
+  ReservoirSampling& operator=(const ReservoirSampling& other);
   ~ReservoirSampling();
-  void insert(double x);
-  double getQuantile(double f);
-  long get_stream_size(){return n;};
+  void insert(double value);
+  double getQuantile(double fraction);
+  long get_stream_size(){return stream_size;};
   
- protected:
-  int n,k;
-  double *s;
+ private:
+  int stream_size, sample_size;
+  double *array;
 
-  void copy(const ReservoirSampling& r);
+  void copy(const ReservoirSampling& other);
 };
+
 #include"ReservoirSampling.cpp"
 #endif
