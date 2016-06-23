0f3eeda39a7dea56e8971286ac925eab64714a55
Farhan Zaki <zaki_f1@denison.edu>
2016-06-23 15:34:49 -0400

    Added the code for header file

--- SourceCode/ChiSquare/ChiSquareCategorical.h
+++ SourceCode/ChiSquare/ChiSquareCategorical.h
@@ -1,1 +1,25 @@
 //ChiSquareCategorical.h
+
+#ifndef CHI_SQUARED_CATIGORICAL_H
+#define CHI_SQUARED_CATIGORICAL_H
+
+#include <iostream>
+#include <stdlib.h> 
+#include<math.h>
+
+// Inserts continuous data into a quantile sketch and computes the chi-squared statistic 
+class ChiSquareCatigorical
+{
+public:
+	ChiSquareCatigorical();
+	ChiSquareCatigorical(double m);
+	~ChiSquareCatigorical();
+  	
+	void insert(double val);
+	double calculate_statistic(const ChiSquareCategorical& second_distribution)
+private:
+	double chi_squared;
+
+class IndexError {};
+#endif
+
