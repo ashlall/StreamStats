e6cc3d0f29f49ad93411c716ae50e894c25017eb
Farhan Zaki <zaki_f1@denison.edu>
2016-06-23 15:52:22 -0400

    Set up a layout for the cpp file

--- SourceCode/ChiSquare/ChiSquareCategorical.cpp
+++ SourceCode/ChiSquare/ChiSquareCategorical.cpp
@@ -1,1 +1,36 @@
 //ChiSquareCategorical.cpp
+
+#include "ChiSquareCategorical.h"
+
+/*
+
+*/
+ChiSquareCatigorical::ChiSquareCatigorical()
+{
+	chi_squared=0;
+	num_bins=50;
+	count= new int[num_bins];
+	std::fill_n(count,num_bins,0); // run a for loop if this doesnt work
+	
+}
+
+/*
+
+
+*/
+void ChiSquareCatigorical::insert( double c)
+{
+	
+	
+	
+}
+
+/*
+
+*/
+double ChiSquareCatigorical::calculate_statistic(const ChiSquareCategorical& second_distribution)
+{
+	
+	
+}
+
