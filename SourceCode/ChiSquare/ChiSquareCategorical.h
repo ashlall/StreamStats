5e40abc0d4ae17e70a65122270d4055d91c24315
Farhan Zaki <zaki_f1@denison.edu>
2016-06-23 16:18:26 -0400

    fixed a big and made a new initializations

--- SourceCode/ChiSquare/ChiSquareCategorical.h
+++ SourceCode/ChiSquare/ChiSquareCategorical.h
@@ -2,10 +2,13 @@
 
 #ifndef CHI_SQUARED_CATIGORICAL_H
 #define CHI_SQUARED_CATIGORICAL_H
-
+#include <algorithm> 
 #include <iostream>
+#include <string>
+#include <unordered_map>
 #include <stdlib.h> 
 #include<math.h>
+using namespace std;
 
 // Inserts continuous data into a quantile sketch and computes the chi-squared statistic 
 class ChiSquareCatigorical
@@ -19,6 +22,10 @@ public:
 	double calculate_statistic(const ChiSquareCategorical& second_distribution)
 private:
 	double chi_squared;
+	int num_bins;
+	int *count;
+	int stream_size;
+	unordered_map<double,double> hash_table;
 
 class IndexError {};
 #endif
