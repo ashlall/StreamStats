ed0a28dc53dbb93ed4cb698c00e57bbd5f2891d7
Farhan Zaki <zaki_f1@denison.edu>
2016-06-23 16:18:05 -0400

    Added the un-ordered map

--- SourceCode/ChiSquare/ChiSquareCategorical.cpp
+++ SourceCode/ChiSquare/ChiSquareCategorical.cpp
@@ -3,40 +3,45 @@
 #include "ChiSquareCategorical.h"
 
 /*
-
+Pre-Condition:
+Post-Condition:
 */
 ChiSquareCatigorical::ChiSquareCatigorical()
 {
 	chi_squared=0;
 	num_bins=50;
+	stream_size=0;
 	count= new int[num_bins];
 	std::fill_n(count,num_bins,0); // run a for loop if this doesnt work
 	
 }
-
+/*
+Pre-Condition:
+Post-Condition:
+*/
 ChiSquareCatigorical::~ChiSquareCatigorical()
 {
 	delete []count; 	
 }
 
-
 /*
-
-
+Pre-Condition:
+Post-Condition:
 */
-void ChiSquareCatigorical::insert( double c)
+void ChiSquareCatigorical::insert(double c)
 {
 	
-	
+	int bin= hash_table[c];
+	count[bin]++;
 	
 }
-
 /*
-
+Pre-Condition:
+Post-Condition:
 */
 double ChiSquareCatigorical::calculate_statistic(const ChiSquareCategorical& second_distribution)
 {
 	
 	
+	
 }
-
