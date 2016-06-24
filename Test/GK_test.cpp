cf3d6fe89d7e270a200dd8b622eba9778bb161ad
Larry Li <li_j8@denison.edu>
2016-06-24 16:36:40 -0400

    modify test cases of GK sketch

--- Test/GK_test.cpp
+++ Test/GK_test.cpp
@@ -2,6 +2,7 @@
 #include<cassert>
 #include <iostream>
 #include <random>
+#include <math.h>    
 
 #include "../SourceCode/Quantiles/GK.cpp"
 
@@ -9,95 +10,93 @@
 
 
 //Input: Stream size, sample size
-//Output: Nothing. Will exist if the following test did not pass.
+//Output: It will test the function of insert, get_stream_size, and getQuantile.
+//It will return nothing if it passes. It Will exist if the following test did not pass.
 void test_GK(int stream_size, int sample_size)
 {
-	int k = sample_size; 
-	long quantA,quantB,quantC,quantD,quantE,quantF,quantG,quantH;
+	double epsilon = log(stream_size)/sample_size;
+	
+	double quantA,quantB,quantC,quantD,quantE,quantF,quantG,quantH;
 	bool condA,condB,condC,condD,condE,condF,condG,condH;
 	double upper,lower;
-	int n=stream_size; 
 	
 	int random_fix1 = rand()% RAND_MAX;
  	int random_fix2 = rand()% RAND_MAX;
  	int random_fix3 = rand()% RAND_MAX;
         
-    	GK a(k);
- 	GK b(k);
-	GK c(k);
-	GK d(k);
- 	GK e(k);
-	GK f(k);
-	GK g(k);
- 	GK h(k);
+    	GK a(sample_size);
+ 	GK b(sample_size);
+	GK c(sample_size);
+	GK d(sample_size);
+ 	GK e(sample_size);
+	GK f(sample_size);
+	GK g(sample_size);
+ 	GK h(sample_size);
 
 	
-	for (double j = 0; j <= n; j++) //inserting in decreasing order
+	for (double j = 0; j <= stream_size; j++) //inserting in decreasing order
    	{	
   		a.insert(j); 
     	}
     	bool condA1;
 	int LA = a.get_stream_size(); //test the function get_stream_size()
-	condA1= (LA == n+1);
+	condA1= (LA == stream_size+1);
 	assert(condA1 == true);
 
-	for (int i = n; i >= 0; i--) //inserting in desceding order
+	for (double i = stream_size; i >= 0; i--) //inserting in desceding order
     	{
   		b.insert(i); 
     	}
     	bool condB1;
 	int LB = b.get_stream_size(); //test the function get_stream_size()
-	condB1= (LB == n+1);
+	condB1= (LB == stream_size+1);
 	assert(condB1 == true);
 
+	/*
+	Generate three uniform distribution with the following range. In order to have a "good" uniform distribution, recommend large stream size.
+	*/
+	int uni_lower_bound1 = 500;
+	int uni_upper_bound1 = 1000;
+
+	int uni_lower_bound2 = 1234;
+	int uni_upper_bound2 = 5678;
+
+	int uni_lower_bound3 = 0;
+	int uni_upper_bound3 = 5000;
 	for (int m = 1; m <= 3; m++) //create 3 "random" sequences that holds their randomness every time runs the code
   	{
-  		 if (m == 1)
-  		 {
-  		   	 //cout << "sequence 1: " << endl;
-  			 for (int i = 0; i <= n; i++)
-   			 {
-   			 	srand(i);
-  				long rand_num=rand()%n;
-  				//cout << rand_num << " ";
-  				c.insert(rand_num);  //inserting float number from 0 to n
-  			 }
-  			 //cout << ""<<endl;
-  			 bool condC1;
-			 int LC = c.get_stream_size(); //test the function get_stream_size()
-			 condC1= (LC == n+1);
-			 assert(condC1 == true);
+		double data;
+  		if (m == 1)
+  		{	
+			default_random_engine generator(5);
+			uniform_real_distribution<double> distribution(uni_lower_bound1,uni_upper_bound1);
+			for (int i = 0; i <= stream_size; i++)
+			{
+   				data = distribution(generator);
+  				c.insert(data);
+			} 
   		}
   		
   		else if(m == 2)
   		{
-  		 	  //cout << "sequence 2: " << endl;
-  			  for (int i = 0; i <= n; i++)
-   			 {
-   			    srand(i^2);
-  				long rand_num=rand()%n;
-  				//cout << rand_num << " ";
-  				d.insert(rand_num); //inserting float number from 0 to n
-  			 }  
-  			 //cout << ""<<endl;
-  			 bool condD1;
-			 int LD = d.get_stream_size(); //test the function get_stream_size()
-			 condD1= (LD == n+1);
-			 assert(condD1 == true);  
+			default_random_engine generator(10);
+			uniform_real_distribution<double> distribution(uni_lower_bound2,uni_upper_bound2);
+			for (int i = 0; i <= stream_size; i++)
+			{
+   				data = distribution(generator);
+  				d.insert(data);
+			}
   		}	
   		
   		else 
   		{	
-  			  for (int i = 0; i <= n; i++)
-   			 {
-   			 	srand(n-i);
-  				long rand_num=rand()%n;
-  				e.insert(rand_num); //inserting float number from 0 to n
-  			 }  
-  			 bool condE1;
-			 int LE = e.get_stream_size(); //test the function get_stream_size()
-			 condE1= (LE == n+1);
-			 assert(condE1 == true);
+			default_random_engine generator(20);
+			uniform_real_distribution<double> distribution(uni_lower_bound3,uni_upper_bound3);
+			for (int i = 0; i <= stream_size; i++)
+			{
+   				data = distribution(generator);
+  				e.insert(data);
+			}
   		}
   	}
   	
@@ -115,31 +114,36 @@ void test_GK(int stream_size, int sample_size)
 	  assert(condG1 == true);
 	  assert(condH1 == true);  
 	  	
-  for (float i=1;i<=100;i++)
+  for (float i=1;i<=stream_size;i++)
   {
-  	  double interval=i/100; //getting each probabilty interval
+  	  double interval=i/stream_size; //getting each probabilty interval
   	  
   	  quantA=a.getQuantile(interval); //getting quantile for each probability interval
-  	  quantB=b.getQuantile(interval);//getting quantile for each probability interval
-  	  quantC=c.getQuantile(interval);//getting quantile for each probability interval
+  	  quantB=b.getQuantile(interval);
+  	  quantC=c.getQuantile(interval);
   	  quantD=d.getQuantile(interval);
   	  quantE=e.getQuantile(interval);
   	  quantF=f.getQuantile(interval);
   	  quantG=g.getQuantile(interval);
   	  quantH=h.getQuantile(interval);
   	  
-  	  lower=(interval-0.04)*n;
-  	  upper=(interval+0.04)*n;
-  	  
+  	  lower=(interval-epsilon)*stream_size;//Used for first two cases.
+  	  upper=(interval+epsilon)*stream_size;
+  	  //cout << "l: " << lower << "  " << "u: " << upper <<endl;
+
   	  condA= ((lower<=quantA) && (upper>=quantA));
-  	  //checking if quantile is within error range
-  	  
   	  condB= ((lower<=quantB) && (upper >=quantB));
   	  //checking if quantile is within error range
   	  
-  	  condC= ((lower<=quantC) && (upper>=quantC));
-  	  condD= ((lower<=quantD) && (upper>=quantD));
-  	  condE= ((lower<=quantE) && (upper>=quantE));
+	
+	 // cout << "cao:" << quantD<<endl;
+	  //cout << (uni_lower_bound2 + (uni_upper_bound2-uni_lower_bound2)*interval)+epsilon*stream_size << endl;
+ 	 //cout << (uni_lower_bound2 + (uni_upper_bound2 - uni_lower_bound2)*interval)-epsilon*stream_size << endl;
+	 //cout << "" <<endl;
+
+	 condC = (quantC >= (uni_lower_bound1+(uni_upper_bound1 - uni_lower_bound1)*interval)- epsilon*stream_size) && (quantC <= (uni_lower_bound1+(uni_upper_bound1 - uni_lower_bound1)*interval) + epsilon*stream_size);
+	 condD = (quantD >= (uni_lower_bound2+(uni_upper_bound2 - uni_lower_bound2)*interval)- epsilon*stream_size) && (quantD <= (uni_lower_bound2+(uni_upper_bound2 - uni_lower_bound2)*interval) + epsilon*stream_size);
+	 condE = (quantE >= (uni_lower_bound3+(uni_upper_bound3 - uni_lower_bound3)*interval)- epsilon*stream_size) && (quantE <= (uni_lower_bound3+(uni_upper_bound3 - uni_lower_bound3)*interval) + epsilon*stream_size);
   	  //checking if quantile is within error range
   	  
   	  
@@ -161,11 +165,14 @@ void test_GK(int stream_size, int sample_size)
 }
 
 //Input: Stream size, sample size
-//Output: Nothing. Will exist if the following test did not pass.
+//Output: It will test the function of reverseQuantile.
+//It will return nothing if it passes. It Will exist if the following test did not pass.
 void test_reverseQuantile(double stream_size, double sample_size)
 {
 	GK m(sample_size);
 	GK n(sample_size);
+	double epsilon = log(stream_size)/sample_size;
+	//cout << "e: " << error<<endl;
 	bool check1, check2;
 	double hold1, hold2;
 
@@ -191,48 +198,29 @@ void test_reverseQuantile(double stream_size, double sample_size)
 	}
 
 	//Theoratically, the reverse quantile lookup will generate 3epsilon.
+
 	else
 	{
 		for(double i = 0; i<=stream_size; i++)
 		{ 
 			hold1 = ((m.reverseQuantile(i, stream_size))/stream_size);
-			check1 = ((hold1 >= i/stream_size - (1/stream_size)*3) &&  (hold1 <= i/stream_size + (1/stream_size)*3));
+			check1 = ((hold1 >= i/stream_size - 3*epsilon) &&  (hold1 <= i/stream_size + 3*epsilon));
 			assert(check1==true);
 
 			hold2 = ((n.reverseQuantile(i, stream_size))/stream_size);
-			check2 = ((hold2 >= i/stream_size - (1/stream_size)*3) &&  (hold2 <= i/stream_size + (1/stream_size)*3));
+			check2 = ((hold2 >= i/stream_size - 3*epsilon) &&  (hold2 <= i/stream_size + 3*epsilon));
 			assert(check2==true);
 		}			
 	}		
 }		
 
-void hello()
-{
-	GK a(1000);
-	GK b(1000);
-	for(int i = 0; i<10000; i++)
-	{
-		a.insert(i);
-		b.insert(i);
-	}
-}
 
 int main()
 {	
-	//Cant run two functions together. Need to fix.
-	test_GK(10000, 10000);
-	test_reverseQuantile(5000,3000); 
-	//hello();
+	test_GK(50000, 8000);
+	test_reverseQuantile(5000,5000); 
  	return 0;
 }
-
-
-
-
-
-
-
-
 
 
 
