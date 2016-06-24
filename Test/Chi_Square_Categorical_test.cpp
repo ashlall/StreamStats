a74eb650c794dcaf68df897501d331e95b5d14d0
Farhan Zaki <zaki_f1@denison.edu>
2016-06-24 12:24:37 -0400

    Adding the test file

---
+++ Test/Chi_Square_Categorical_test.cpp
@@ -0,0 +1,22 @@
+//categorical_test.cpp
+#include "../SourceCode/ChiSquare/ChiSquareCategorical.h"
+
+void cat_test()
+{
+	ChiSquareCategorical c1();
+	ChiSquareCategorical c2();	
+	
+	for(int i=0;i<=10000;i++)
+	{
+		c1.insert(i);
+		c2.insert(i);
+	}
+	
+	cout<<c1.calculate_statistic(c2)<<endl;
+
+
+int main()
+{	
+	cat_test();			
+	return 0;		
+}
