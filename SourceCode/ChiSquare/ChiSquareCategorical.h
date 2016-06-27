94478525c657259bdbb569f2e107a23ef1fb0f57
Farhan Zaki <zaki_f1@denison.edu>
2016-06-27 15:11:09 -0400

    Fixed som bugs

--- SourceCode/ChiSquare/ChiSquareCategorical.h
+++ SourceCode/ChiSquare/ChiSquareCategorical.h
@@ -2,19 +2,40 @@
 
 #ifndef CHI_SQUARED_CATEGORICAL_H
 #define CHI_SQUARED_CATEGORICAL_H
+
 #include <algorithm> 
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <stdlib.h> 
+#include<stdio.h>
 #include<math.h>
 using namespace std;
+
+class HashTable
+{
+public:
+	int rand_num;
+	HashTable();
+	HashTable(int n)
+	{
+		rand_num=n;
+	}
+	int hash(int x)
+	{
+		x = ((x >> 16) ^ x) * 0x45d9f3b;
+		x = ((x >> 16) ^ x) * 0x45d9f3b;
+		x = ((x >> 16) ^ x);
+    return x;
+	}
+};
 
 // Inserts Categorical data into a quantile sketch and computes the chi-squared statistic 
 class ChiSquareCategorical
 {
 public:
 	ChiSquareCategorical();
+	ChiSquareCategorical(int buckets);
 	~ChiSquareCategorical();
   	
 	void insert(double val);
@@ -25,7 +46,7 @@ private:
 	int num_buckets;
 	int *count;
 	int stream_size;
-	unordered_map<double,double> hash_table;
-
+	unordered_map<double,int> hash_table;
 };
+
 #endif
