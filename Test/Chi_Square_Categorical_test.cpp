0b3e373d231639dd03ce143f5826b2f72a8970d7
Farhan Zaki <zaki_f1@denison.edu>
2016-06-28 13:38:22 -0400

    test

--- Test/Chi_Square_Categorical_test.cpp
+++ Test/Chi_Square_Categorical_test.cpp
@@ -1,6 +1,7 @@
 //Chi_Square_Categorical.cpp
 #include <iostream>
 #include <random>
+#include<cassert>
 
 #include "../SourceCode/ChiSquare/ChiSquareCategorical.cpp"
 using namespace std;
@@ -10,21 +11,41 @@ void categorical_test1()
 	ChiSquareCategorical c2(100);
 	for(int j=0;j<10;j++)
 	{ 
-	for(int i=0;i<10000;i++)
+	for(int i=0;i<100000;i++)
 	{
 		c1.insert(i);
 	}
-	for(int i=9999;i>=0;i--)
+	for(int i=99999;i>=0;i--)
 	{
 		c2.insert(i);
 	}
 	
 	}
-	cout<<c1.calculate_statistic(c2)<<endl;
+	assert(c1.calculate_statistic(c2)==0);
 }
+
+void categorical_test2()
+{
+	ChiSquareCategorical c1(100);
+	ChiSquareCategorical c2(100);
+	
+	default_random_engine generator(5);
+	normal_distribution<double> distribution(10000,1000);
+	
+	for(int i=0; i<1000000;i++)
+	{
+		double data=distribution(generator);
+		c1.insert(i);
+		c2.insert(i);
+		
+	}
+	assert(c1.calculate_statistic(c2)==0);
+}
+
+
 int main()
 {	
 	categorical_test1();
-	//categorical_test2();
+	categorical_test2();
 	return 0;		
 }
