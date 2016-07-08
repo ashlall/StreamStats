463a4927dd79ca58b4f64f652ce0e8be3c2c3308
Farhan Zaki <zaki_f1@denison.edu>
2016-07-08 13:43:56 -0400

    new categorical algoritm

--- SourceCode/ChiSquare/ChiSquareCategorical.cpp
+++ SourceCode/ChiSquare/ChiSquareCategorical.cpp
@@ -4,11 +4,12 @@
 Pre-Condition: None.
 Post-Condition:Initializes the variables used in the ChiSquareCategorical class.
 */
-ChiSquareCategorical::ChiSquareCategorical(int buckets)
+ChiSquareCategorical::ChiSquareCategorical(double p)
 {
 	chi_squared=0;
-	num_buckets=buckets;
 	stream_size=0;
+	percent =p;
+	range=2^32;
 	count= new int[num_buckets];
 	std::fill_n(count,num_buckets,0); // run a for loop if this doesnt work 
 	
@@ -35,9 +36,22 @@ void ChiSquareCategorical::insert(double c)
 	// Passing c into the hash function and increases the count of
 	// whichever bucket it falls into. 
 	HashTable HashA(71);
-	int bucket= int((HashA.hash(c))%num_buckets);	
-	//int bucket= int(hash_table[c]) % num_buckets;
-	count[bucket]++;
+	/*int bucket= int((HashA.hash(c))%num_buckets);	
+	count[bucket]++;*/
+	
+	long hash_value= HashA.hash(c);;
+	if(hash_value<(percent*range))
+	{	
+		if(map.count(hash_value)<=1)
+			map.emplace(hash_value,1);
+		else
+		{
+			long cat_count=map.at(hash_value);
+			map.at(hash_value)=cat_count++;
+		}
+	}
+		
+	
 	
 	
 }
@@ -46,7 +60,7 @@ Pre-Condition: distribution_2 the second stream of data which has used the
 same number of buckets and hash function.
 Post-Condition: Returns the Chi-Squared statistic for the categorical data.
 */
-double ChiSquareCategorical::calculate_statistic(const ChiSquareCategorical& second_distribution,int &df)
+double ChiSquareCategorical::calculate_statistic(const ChiSquareCategorical& second_distribution)
 {	
 	//Getting the size of both streams
 	int stream_size_1=stream_size;
@@ -56,12 +70,13 @@ double ChiSquareCategorical::calculate_statistic(const ChiSquareCategorical& sec
 	double constant_1 = sqrt((double)stream_size_2/stream_size_1);
 	double constant_2 = sqrt((double)stream_size_1/stream_size_2);
 	
+	int df=map.size();
 	
-	for (int i=0;i<num_buckets;i++)
-	{	
+	for ( auto it = map.begin(); it != map.end(); ++it )
+	{
 		 // Getting the frequencies if the i'th bin from the two streams.
-		 double frequency_1= count[i];
-		 double frequency_2= second_distribution.count[i];
+		 double frequency_1= 
+		 double frequency_2= 
 		 
 		 if((frequency_1==0) && (frequency_2==0))
 		 df--;
