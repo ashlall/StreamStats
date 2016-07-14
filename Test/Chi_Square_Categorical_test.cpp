0f2e08372cf2eef532b6cef9f0efe2ae59ceb541
Ashwin Lall <lalla@lalla.mathsci.denison.edu>
2016-07-14 14:10:52 -0400

    Updated categorical test file to use power-law data and repeat until p value is in given range

--- Test/Chi_Square_Categorical_test.cpp
+++ Test/Chi_Square_Categorical_test.cpp
@@ -116,70 +116,98 @@ void categorical_test3()
 	cout<<pochisq(chi,num_bins-1)<<endl;
 	
 }*/
+
+
+// returns a random sample (one of n categories from 0 to n-1)
+// when cumulative sum of frequencies of items in each categories 
+// is in cumulative_freq
+int sample(int n, int cumulative_freq[])
+{
+    int random = rand() % cumulative_freq[n-1], i = 0; 
+    while (cumulative_freq[i] < random) { i++; }
+    return i;
+}
+
+
 void categorical_test4()
 {	int seed=0;
-	double memory_percent=20;	
-	for(int js=0;js<20;js++)
+	double memory_percent=10;
+	int stream_size1 = 1000000;
+	int stream_size2 = stream_size1;
+	int num_cat = 10000;
+	int cumulative_freq1[num_cat];
+	int cumulative_freq2[num_cat];
+
+	// generate a Zipfian distribution with num_cat categories
+	int sum1 = 0, sum2 = 0;
+	for(int i = 0; i < num_cat; ++i)
 	{
-		double num_cat=1000000;
-		int df1=num_cat;
-		int df2=0;
-		int N,M;
-		double actual_chi;
-		ChiSquareCategorical c1(memory_percent);
-		ChiSquareCategorical c2(memory_percent);
+	    sum1 += max((int)(num_cat/pow(i+1, 1.1)), 1); 
+	    cumulative_freq1[i] = sum1;
+
+	    sum2 += max((int)(num_cat/pow(i+1, 1.105)), 1);
+            cumulative_freq2[i] = sum2; 
+
+	}
+
+	for(int repeat=0;repeat<10;repeat++)
+	{
+	    double actual_chi;
+	    ChiSquareCategorical c1(memory_percent);
+	    ChiSquareCategorical c2(memory_percent);
+	    
+	    double p_value = 0;
+	    int count_1[num_cat], count_2[num_cat];
+	    
+	    while (p_value < 0.02 || p_value > .1)
+	    {			
+		std::fill_n(count_1,num_cat,1);
+		std::fill_n(count_2,num_cat,1);
 		
-		double p_value=0;
-		int max_dif;
-		int count_1[(int)num_cat];int count_2[(int)num_cat];
-			
-		actual_chi=0;
-		N=0;
-		M=0;
-		std::fill_n(count_1,num_cat,0);
-		std::fill_n(count_2,num_cat,0);
-		double stream_size1=0; 
-		double stream_size2=0; 
 		srand(seed);
-			
-		for(int i=0;i<num_cat;i++)
-		{
-			int v1= (int)(rand() % 250+50); // See between %35-40 with +100
-			max_dif= 28;// see with 28
-			int v2= v1-max_dif+rand()%(2*max_dif+1);
-				
-			count_1[i]=v1;
-			count_2[i]=v2;
-				
-			stream_size1=stream_size1+v1;
-			stream_size2=stream_size2+v2;
-		}
+		
+		for(int j=0; j < stream_size1; ++j)
+		    count_1[sample(num_cat, cumulative_freq1)]++;
+		
+		for(int j=0; j < stream_size2; ++j)
+		    count_2[sample(num_cat, cumulative_freq2)]++;
+		
+		//for(int j=0; j < 10; ++j)
+		//	cout << count_1[j] << " " << count_2[j] << endl;
+		
 		double constant_1 = sqrt((double)stream_size2/stream_size1);
 		double constant_2 = sqrt((double)stream_size1/stream_size2);
-			
-			
+		
+		actual_chi = 0.0;
 		for(int i=0;i<num_cat;i++)
 		{
-			double frequency_1=count_1[i];
-			double frequency_2=count_2[i];
-			double value = frequency_1 * constant_1 - frequency_2 * constant_2;
-			actual_chi += (value * value) / (frequency_1 + frequency_2);
+		    double frequency_1=count_1[i];
+		    double frequency_2=count_2[i];
+		    double value = frequency_1 * constant_1 - frequency_2 * constant_2;
+		    actual_chi += (value * value) / (frequency_1 + frequency_2);
 		}
-			
-		p_value=1-pochisq(actual_chi,df1--);
-		seed=seed+1;
-			
-		for(int i=0;i<num_cat;i++)
-		{
-			for(int j=0;j<count_1[i];j++)
-				c1.insert(i);
-				
-			for(int k=0;k<count_2[i];k++)
-				c2.insert(i);
-		}
-		double chi=c1.calculate_statistic(c2,df2);
-		cout<<"Actual: "<<p_value<<endl;
-		cout<<"Calculated: "<<1-pochisq(chi,df2--)<<endl;
+		
+		p_value=pochisq(actual_chi,df1);
+		//cout << actual_chi << " " << p_value << endl;
+		seed=seed+1;  
+	    }
+	    
+	    for(int i=0;i<num_cat;i++)
+	    {
+		for(int j=0;j<count_1[i];j++)
+		    c1.insert(i);
+		
+		for(int k=0;k<count_2[i];k++)
+		    c2.insert(i);
+	    }
+
+	    double chi=c1.calculate_statistic(c2);
+	    
+	    cout << "Actual stat:" << actual_chi << endl;
+	    cout << "Calculated :" << chi << endl;
+	    
+	    cout<<"Actual   p: "<<p_value<<endl;
+	    cout<<"Calculated: "<<pochisq(chi,df1) <<endl << endl;
 	}
 }
 
