68b2104be2f6415184d78dd9cc34c3efbd25d045
Ashwin Lall <lalla@lalla.mathsci.denison.edu>
2016-07-15 11:15:43 -0400

    Changed categorical test back to coordinated sampling

--- SourceCode/ChiSquare/ChiSquareCategorical.cpp
+++ SourceCode/ChiSquare/ChiSquareCategorical.cpp
@@ -51,38 +51,42 @@ double ChiSquareCategorical::calculate_statistic(const ChiSquareCategorical& sec
 	double constant_1 = sqrt((double)stream_size_2/stream_size_1);
 	double constant_2 = sqrt((double)stream_size_1/stream_size_2);
 	double estimateSum = 0.0;
+	int count = 0;
 	for ( auto it = map.begin(); it != map.end(); ++it )
 	{
 		 // Getting the frequencies if the i'th bin from the two streams.
-		 double frequency_1= it->second;
-		 double frequency_2= second_distribution.map.at(it->first);
+		 int frequency_1= it->second;
+		 int frequency_2= second_distribution.map.at(it->first);
 	 	
 		 //Calculating the Chi-Squared Statistic
 		
 		 // Naive sampling:
 		 double value = frequency_1 * constant_1 - frequency_2 * constant_2;
 		 chi_squared += (100.0/percent) * (value * value) / (frequency_1 + frequency_2);
-
+		 
+		 
 		 /*
 		 // AMS-sampling from https://people.cs.umass.edu/~mcgregor/papers/07-unsketchables.pdf
 		 int r, s;
+		 double X;
 		 if (rand() % 2 == 0) // randomly pick between stream 1 and stream 2
 		 {
 		     r = (1 + rand() % stream_size_2) + (rand() % frequency_1) * stream_size_2;
 		     s = frequency_2 * stream_size_1;
+		     X = 2.0 * ((double)(r * s) / (r + s) - ((double)(r - 1)*s)/(r + s - 1));  
 		 }
 		 else
 		 {
 		     r = 0;
-		     s = (1 + rand() % stream_size_1) + (rand() % frequency_2) * stream_size_1; 
+		     s = (1 + rand() % stream_size_1) + (rand() % frequency_2) * stream_size_1;
+		     X = 2.0 * ((double)(r * s) / (r + s) - ((double)r*(s - 1))/(r + s - 1));
 		 }
+		 
+		 estimateSum += X;
+		 count++;
 		 */
-		 
-
-		 
-
-
 	}
 	
 	return chi_squared;
+	//return (stream_size_1 + stream_size_2 - 4 * stream_size_1 * estimateSum / count);
 }
